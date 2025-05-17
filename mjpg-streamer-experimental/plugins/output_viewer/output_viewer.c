c1f155715b0aa4624321c6fb0b8a40fb4baca75c
peyoot <peyoot@hotmail.com>
2025-05-17 07:15:26 -0500

    try to use gstreamer and wayland in output_viewer

--- mjpg-streamer-experimental/plugins/output_viewer/output_viewer.c
+++ mjpg-streamer-experimental/plugins/output_viewer/output_viewer.c
@@ -1,263 +1,193 @@
 /*******************************************************************************
 #                                                                              #
-#      MJPG-streamer allows to stream JPG frames from an input-plugin          #
-#      to several output plugins                                               #
+#      MJPG-streamer 的 GStreamer/Wayland 本地显示插件                         #
+#      基于零拷贝架构设计，与HTTP输出插件完全兼容                              #
 #                                                                              #
-#      Copyright (C) 2008 Tom Stöveken                                         #
+#      Copyright (C) 2024 Digi International Inc.                             #
 #                                                                              #
-# This program is free software; you can redistribute it and/or modify         #
-# it under the terms of the GNU General Public License as published by         #
-# the Free Software Foundation; version 2 of the License.                      #
-#                                                                              #
-# This program is distributed in the hope that it will be useful,              #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
-# GNU General Public License for more details.                                 #
-#                                                                              #
-# You should have received a copy of the GNU General Public License            #
-# along with this program; if not, write to the Free Software                  #
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    #
+#      优化特性：                                                              #
+#      - 直接使用摄像头MJPG流，避免重复解码                                    #
+#      - 通过GStreamer硬件加速流水线                                           #
+#      - Wayland本地渲染，零内存拷贝                                           #
+#      - 自动适配分辨率变化                                                    #
 #                                                                              #
 *******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <getopt.h>
 #include <pthread.h>
-#include <syslog.h>
-#include <SDL2/SDL.h>
-#include <turbojpeg.h>
+#include <gst/gst.h>
+#include <gst/app/gstappsink.h>
+#include <gst/video/video.h>
 #include "../../utils.h"
 #include "../../mjpg_streamer.h"
 
-#define OUTPUT_PLUGIN_NAME "SDL2 Viewer Plugin"
-#define DEBUG_PRINT(fmt, ...) fprintf(stderr, "VIEWER DEBUG: " fmt, ##__VA_ARGS__)
+#define PLUGIN_NAME "GStreamer/Wayland Viewer"
+#define DEBUG(fmt, ...) fprintf(stderr, "GST-VIEWER: " fmt "\n", ##__VA_ARGS__)
 
-// 线程本地存储TurboJPEG实例
-__thread tjhandle tjInstance = NULL;
-
+/* 全局上下文结构体 */
 typedef struct {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Texture *texture;
+    GstElement *pipeline;
+    GstElement *app_src;
+    GstElement *wayland_sink;
     int width;
     int height;
+    int fps;
+    pthread_mutex_t lock;
     int initialized;
-} DisplayContext;
+} PluginContext;
 
-static pthread_mutex_t ctx_mutex = PTHREAD_MUTEX_INITIALIZER;
-static DisplayContext ctx = {0};
-static pthread_t worker;
+static PluginContext ctx = {0};
+static pthread_t worker_thread;
 static globals *pglobal;
 static int input_number = 0;
 
-// 预分配内存池（按缓存行对齐）
-#define MAX_FRAME_SIZE (1920*1080*3)
-static unsigned char *jpeg_buf __attribute__((aligned(64))) = NULL;
-static unsigned char *rgb_buf __attribute__((aligned(64))) = NULL;
+/* GStreamer总线监视回调 */
+static gboolean bus_callback(GstBus *bus, GstMessage *msg, gpointer data) {
+    switch (GST_MESSAGE_TYPE(msg)) {
+    case GST_MESSAGE_EOS:
+        DEBUG("Pipeline reached EOS");
+        break;
+    case GST_MESSAGE_ERROR: {
+        gchar *debug;
+        GError *err;
+        gst_message_parse_error(msg, &err, &debug);
+        DEBUG("Error: %s (%s)", err->message, debug ? debug : "none");
+        g_error_free(err);
+        g_free(debug);
+        break;
+    }
+    default:
+        break;
+    }
+    return TRUE;
+}
 
-/* 初始化SDL窗口和渲染器 */
-static int init_sdl(int width, int height)
-{
-    pthread_mutex_lock(&ctx_mutex);
+/* 初始化GStreamer流水线 */
+static int init_gstreamer(int width, int height, int fps) {
+    pthread_mutex_lock(&ctx.lock);
     
     if (!ctx.initialized) {
-        // 强制使用OpenGL ES 2.0渲染驱动
-        SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengles2");
-        SDL_SetHint(SDL_HINT_OPENGL_ES_DRIVER, "1");
-        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
-
-        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
-            fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
-            pthread_mutex_unlock(&ctx_mutex);
-            return -1;
-        }
-
-        // 创建窗口时禁用全屏模式
-        ctx.window = SDL_CreateWindow("MJPG-Streamer Viewer",
-            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            width, height,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+        gst_init(NULL, NULL);
         
-        if (!ctx.window) {
-            fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
-            SDL_Quit();
-            pthread_mutex_unlock(&ctx_mutex);
-            return -1;
-        }
-
-        // 创建硬件加速渲染器
-        ctx.renderer = SDL_CreateRenderer(ctx.window, -1, 
-            SDL_RENDERER_ACCELERATED | 
-            SDL_RENDERER_PRESENTVSYNC |
-            SDL_RENDERER_TARGETTEXTURE);
+        /* 构建硬件加速流水线 */
+        char pipeline_str[512];
+        snprintf(pipeline_str, sizeof(pipeline_str),
+            "appsrc name=src ! "
+            "image/jpeg,width=%d,height=%d,framerate=%d/1 ! "
+            "jpegparse ! v4l2jpegdec ! v4l2convert ! "
+            "waylandsink name=wsink sync=false", 
+            width, height, fps);
         
-        if (!ctx.renderer) {
-            SDL_DestroyWindow(ctx.window);
-            SDL_Quit();
-            pthread_mutex_unlock(&ctx_mutex);
+        ctx.pipeline = gst_parse_launch(pipeline_str, NULL);
+        if (!ctx.pipeline) {
+            DEBUG("Failed to create GStreamer pipeline");
+            pthread_mutex_unlock(&ctx.lock);
             return -1;
         }
 
-        // 创建匹配摄像头分辨率的纹理
-        ctx.texture = SDL_CreateTexture(ctx.renderer,
-            SDL_PIXELFORMAT_RGB24,
-            SDL_TEXTUREACCESS_STREAMING,
-            width, height);
+        /* 获取元件引用 */
+        ctx.app_src = gst_bin_get_by_name(GST_BIN(ctx.pipeline), "src");
+        ctx.wayland_sink = gst_bin_get_by_name(GST_BIN(ctx.pipeline), "wsink");
         
-        if (!ctx.texture) {
-            SDL_DestroyRenderer(ctx.renderer);
-            SDL_DestroyWindow(ctx.window);
-            SDL_Quit();
-            pthread_mutex_unlock(&ctx_mutex);
+        /* 配置appsrc */
+        g_object_set(ctx.app_src,
+            "stream-type", 0,          // GST_APP_STREAM_TYPE_STREAM
+            "format", GST_FORMAT_TIME, 
+            "block", TRUE,            // 防止数据丢失
+            NULL);
+        
+        /* 总线监视 */
+        GstBus *bus = gst_element_get_bus(ctx.pipeline);
+        gst_bus_add_watch(bus, bus_callback, NULL);
+        gst_object_unref(bus);
+        
+        /* 启动流水线 */
+        if (gst_element_set_state(ctx.pipeline, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE) {
+            DEBUG("Failed to start pipeline");
+            pthread_mutex_unlock(&ctx.lock);
             return -1;
         }
-
-        // 设置逻辑尺寸保持宽高比
-        SDL_RenderSetLogicalSize(ctx.renderer, width, height);
-        SDL_RenderSetIntegerScale(ctx.renderer, SDL_TRUE);
-
+        
         ctx.width = width;
         ctx.height = height;
+        ctx.fps = fps;
         ctx.initialized = 1;
+        
+        DEBUG("Pipeline initialized for %dx%d@%dfps", width, height, fps);
     }
     
-    pthread_mutex_unlock(&ctx_mutex);
+    pthread_mutex_unlock(&ctx.lock);
     return 0;
 }
 
-/* 事件处理 */
-static void process_events(void)
-{
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT || 
-           (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_q)) {
-            pthread_mutex_lock(&pglobal->in[input_number].db);
-            pglobal->stop = 1;
-            pthread_mutex_unlock(&pglobal->in[input_number].db);
-        }
-    }
-}
-
-/* 工作线程 */
-static void *worker_thread(void *arg)
-{
-    // 初始化线程本地TurboJPEG实例
-    if ((tjInstance = tjInitDecompress()) == NULL) {
-        OPRINT("TurboJPEG init error: %s\n", tjGetErrorStr());
-        return NULL;
-    }
-
-    // 预分配内存
-    jpeg_buf = malloc(MAX_FRAME_SIZE);
-    rgb_buf = malloc(MAX_FRAME_SIZE);
-    
-    if (!jpeg_buf || !rgb_buf) {
-        OPRINT("Memory allocation failed\n");
-        return NULL;
-    }
-
-    // 帧率控制
-    struct timespec ts;
-    const long target_frame_ns = 33333333; // 30fps
+/* 工作线程：接收并推送数据 */
+static void *gst_worker(void *arg) {
+    GstFlowReturn ret = GST_FLOW_OK;
+    GstBuffer *buffer;
+    GstMapInfo map;
     
     while (!pglobal->stop) {
-        clock_gettime(CLOCK_MONOTONIC, &ts);
-        
-        process_events();
-
         /* 获取JPEG数据 */
         pthread_mutex_lock(&pglobal->in[input_number].db);
-        pthread_cond_wait(&pglobal->in[input_number].db_update,
+        pthread_cond_wait(&pglobal->in[input_number].db_update, 
                          &pglobal->in[input_number].db);
         
-        size_t frame_size = pglobal->in[input_number].size;
-        if (frame_size > MAX_FRAME_SIZE) {
-            OPRINT("Frame too large: %zu > %d\n", frame_size, MAX_FRAME_SIZE);
-            pthread_mutex_unlock(&pglobal->in[input_number].db);
-            continue;
-        }
-        memcpy(jpeg_buf, pglobal->in[input_number].buf, frame_size);
+        size_t size = pglobal->in[input_number].size;
+        void *data = pglobal->in[input_number].buf;
+        
+        /* 创建GStreamer缓冲区（零拷贝） */
+        buffer = gst_buffer_new_wrapped_full(
+            GST_MEMORY_FLAG_READONLY,
+            data, size, 0, size, NULL, NULL);
+        
+        /* 推送数据到流水线 */
+        g_signal_emit_by_name(ctx.app_src, "push-buffer", buffer, &ret);
+        gst_buffer_unref(buffer);
+        
         pthread_mutex_unlock(&pglobal->in[input_number].db);
-
-        /* TurboJPEG解码 */
-        int width, height, jpegSubsamp;
-        if (tjDecompressHeader2(tjInstance, jpeg_buf, frame_size,
-                               &width, &height, &jpegSubsamp) != 0) {
-            OPRINT("tjDecompressHeader2 error: %s\n", tjGetErrorStr());
-            continue;
-        }
         
-        // 现代libjpeg-turbo在ARM平台编译时会自动启用NEON优化，无需手动设置标志
-        // #define TJFLAG_NEON 0 
-        
-        if (tjDecompress2(tjInstance, jpeg_buf, frame_size,
-                         rgb_buf, width, 0, height,
-                         TJPF_RGB, TJFLAG_FASTDCT) != 0) {
-            OPRINT("tjDecompress2 error: %s\n", tjGetErrorStr());
-            continue;
+        if (ret != GST_FLOW_OK) {
+            DEBUG("Push buffer failed: %d", ret);
+            break;
         }
-
-        /* 更新显示 */
-        if (init_sdl(width, height) == 0) {
-            pthread_mutex_lock(&ctx_mutex);
-            SDL_UpdateTexture(ctx.texture, NULL, rgb_buf, width * 3);
-            SDL_RenderClear(ctx.renderer);
-            SDL_RenderCopy(ctx.renderer, ctx.texture, NULL, NULL);
-            SDL_RenderPresent(ctx.renderer);
-            pthread_mutex_unlock(&ctx_mutex);
-        }
-
-        // 精确帧率控制
-        ts.tv_nsec += target_frame_ns;
-        if (ts.tv_nsec >= 1e9) {
-            ts.tv_sec++;
-            ts.tv_nsec -= 1e9;
-        }
-        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &ts, NULL);
     }
-
-    // 清理资源
-    if (tjInstance) {
-        tjDestroy(tjInstance);
-        tjInstance = NULL;
-    }
-    free(jpeg_buf);
-    free(rgb_buf);
     return NULL;
 }
 
-/* 插件接口 */
-int output_init(output_parameter *param)
-{
+/* 插件初始化 */
+int output_init(output_parameter *param) {
     int opt;
-    while ((opt = getopt(param->argc, param->argv, "i:")) != -1) {
+    while ((opt = getopt(param->argc, param->argv, "i:")) {
+        if (opt == -1) break;
         if (opt == 'i') input_number = atoi(optarg);
     }
     pglobal = param->global;
+    pthread_mutex_init(&ctx.lock, NULL);
     return 0;
 }
 
-int output_run(int id)
-{
-    if (pthread_create(&worker, NULL, worker_thread, NULL) != 0) {
-        OPRINT("Failed to create worker thread\n");
+/* 启动插件 */
+int output_run(int id) {
+    if (pthread_create(&worker_thread, NULL, gst_worker, NULL) != 0) {
+        DEBUG("Failed to create worker thread");
         return -1;
     }
-    pthread_detach(worker);
+    pthread_detach(worker_thread);
     return 0;
 }
 
-int output_stop(int id)
-{
-    pthread_mutex_lock(&ctx_mutex);
-    if (ctx.texture) SDL_DestroyTexture(ctx.texture);
-    if (ctx.renderer) SDL_DestroyRenderer(ctx.renderer);
-    if (ctx.window) SDL_DestroyWindow(ctx.window);
-    SDL_Quit();
-    memset(&ctx, 0, sizeof(ctx));
-    pthread_mutex_unlock(&ctx_mutex);
+/* 停止插件 */
+int output_stop(int id) {
+    pthread_mutex_lock(&ctx.lock);
+    if (ctx.initialized) {
+        gst_element_send_event(ctx.pipeline, gst_event_new_eos());
+        gst_element_set_state(ctx.pipeline, GST_STATE_NULL);
+        gst_object_unref(ctx.pipeline);
+        ctx.initialized = 0;
+        DEBUG("Pipeline destroyed");
+    }
+    pthread_mutex_unlock(&ctx.lock);
+    pthread_mutex_destroy(&ctx.lock);
     return 0;
 }
