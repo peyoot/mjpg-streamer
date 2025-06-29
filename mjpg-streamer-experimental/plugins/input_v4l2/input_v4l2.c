c5db5c2299534e6bec3874e7001e37a7b8b9ccdc
peyoot <peyoot@hotmail.com>
2025-06-29 01:10:27 -0500

    use other input plugin framwork

--- mjpg-streamer-experimental/plugins/input_v4l2/input_v4l2.c
+++ mjpg-streamer-experimental/plugins/input_v4l2/input_v4l2.c
@@ -1,189 +1,208 @@
 
+/******************************************************************************
+ * input_v4l2.c - MJPG-streamer input plugin using V4L2 interface
+ * Optimized for low-latency streaming and compatible with MJPG-Streamer plugin API
+ ******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <linux/videodev2.h>
+#include <pthread.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <pthread.h>
+#include <linux/videodev2.h>
 
 #include "../../mjpg_streamer.h"
 #include "../../utils.h"
 
-static char *device = "/dev/video0";
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
+
+static globals *pglobal = NULL;
+static int g_running = 0;
+
+typedef struct {
+    void *start;
+    size_t length;
+} buffer_t;
+
+static buffer_t *buffers = NULL;
+static unsigned int n_buffers = 0;
+static int fd = -1;
+static pthread_t capture_thread;
+
+static char *dev_name = "/dev/video0";
 static int width = 640;
 static int height = 480;
 static int fps = 30;
-static int force_yuyv = 0;
+static int use_yuyv = 0;
 
-static int fd = -1;
-static void *buffer_start = NULL;
-static size_t buffer_length = 0;
-static pthread_t capture_thread;
-static int stop_capturing = 0;
+static void errno_exit(const char *s) {
+    fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
+    exit(EXIT_FAILURE);
+}
 
-static int xioctl(int fd, int request, void *arg) {
+static int xioctl(int fh, int request, void *arg) {
     int r;
     do {
-        r = ioctl(fd, request, arg);
+        r = ioctl(fh, request, arg);
     } while (r == -1 && errno == EINTR);
     return r;
 }
 
 static void *capture_loop(void *arg) {
-    while (!stop_capturing) {
-        fd_set fds;
-        struct timeval tv;
-        int r;
+    struct v4l2_buffer buf;
 
-        FD_ZERO(&fds);
-        FD_SET(fd, &fds);
-
-        tv.tv_sec = 2;
-        tv.tv_usec = 0;
-
-        r = select(fd + 1, &fds, NULL, NULL, &tv);
-        if (r == -1) {
-            perror("select");
-            break;
-        }
-
-        struct v4l2_buffer buf;
-        memset(&buf, 0, sizeof(buf));
+    while (g_running) {
+        CLEAR(buf);
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory = V4L2_MEMORY_MMAP;
 
         if (xioctl(fd, VIDIOC_DQBUF, &buf) == -1) {
+            if (errno == EAGAIN) continue;
             perror("VIDIOC_DQBUF");
             break;
         }
 
-        callback_new_frame(buffer_start + buf.m.offset, buf.bytesused);
+        if (pglobal && pglobal->callback) {
+            pglobal->callback((unsigned char *)buffers[buf.index].start, buf.bytesused);
+        }
 
         if (xioctl(fd, VIDIOC_QBUF, &buf) == -1) {
             perror("VIDIOC_QBUF");
             break;
         }
     }
+
     return NULL;
 }
 
-int input_init(input_parameter *param, int id) {
+static void init_device() {
+    struct v4l2_capability cap;
     struct v4l2_format fmt;
     struct v4l2_requestbuffers req;
-    struct v4l2_buffer buf;
-    struct v4l2_streamparm streamparm;
 
-    fd = open(device, O_RDWR);
+    if (xioctl(fd, VIDIOC_QUERYCAP, &cap) == -1) {
+        errno_exit("VIDIOC_QUERYCAP");
+    }
+
+    CLEAR(fmt);
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmt.fmt.pix.width = width;
+    fmt.fmt.pix.height = height;
+    fmt.fmt.pix.pixelformat = use_yuyv ? V4L2_PIX_FMT_YUYV : V4L2_PIX_FMT_MJPEG;
+    fmt.fmt.pix.field = V4L2_FIELD_ANY;
+
+    if (xioctl(fd, VIDIOC_S_FMT, &fmt) == -1) {
+        errno_exit("VIDIOC_S_FMT");
+    }
+
+    CLEAR(req);
+    req.count = 4;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+
+    if (xioctl(fd, VIDIOC_REQBUFS, &req) == -1) {
+        errno_exit("VIDIOC_REQBUFS");
+    }
+
+    buffers = calloc(req.count, sizeof(buffer_t));
+    for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
+        struct v4l2_buffer buf;
+        CLEAR(buf);
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = n_buffers;
+
+        if (xioctl(fd, VIDIOC_QUERYBUF, &buf) == -1) {
+            errno_exit("VIDIOC_QUERYBUF");
+        }
+
+        buffers[n_buffers].length = buf.length;
+        buffers[n_buffers].start = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+        if (buffers[n_buffers].start == MAP_FAILED) {
+            errno_exit("mmap");
+        }
+    }
+
+    for (unsigned int i = 0; i < n_buffers; ++i) {
+        struct v4l2_buffer buf;
+        CLEAR(buf);
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if (xioctl(fd, VIDIOC_QBUF, &buf) == -1) {
+            errno_exit("VIDIOC_QBUF");
+        }
+    }
+
+    enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (xioctl(fd, VIDIOC_STREAMON, &type) == -1) {
+        errno_exit("VIDIOC_STREAMON");
+    }
+}
+
+int input_init(input_parameter *param, int id) {
+    pglobal = param->global;
+    g_running = 1;
+
+    // Parse parameters
+    char *arg = strtok(param->parameter_string, " ");
+    while (arg) {
+        if (strcmp(arg, "-d") == 0) {
+            arg = strtok(NULL, " ");
+            if (arg) dev_name = strdup(arg);
+        } else if (strcmp(arg, "-r") == 0) {
+            arg = strtok(NULL, " ");
+            if (arg) sscanf(arg, "%dx%d", &width, &height);
+        } else if (strcmp(arg, "-f") == 0) {
+            arg = strtok(NULL, " ");
+            if (arg) fps = atoi(arg);
+        } else if (strcmp(arg, "-y") == 0) {
+            use_yuyv = 1;
+        }
+        arg = strtok(NULL, " ");
+    }
+
+    fd = open(dev_name, O_RDWR | O_NONBLOCK, 0);
     if (fd == -1) {
         perror("Opening video device");
         return 1;
     }
 
-    memset(&fmt, 0, sizeof(fmt));
-    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    fmt.fmt.pix.width = width;
-    fmt.fmt.pix.height = height;
-    fmt.fmt.pix.pixelformat = force_yuyv ? V4L2_PIX_FMT_YUYV : V4L2_PIX_FMT_MJPEG;
-    fmt.fmt.pix.field = V4L2_FIELD_ANY;
+    init_device();
 
-    if (xioctl(fd, VIDIOC_S_FMT, &fmt) == -1) {
-        perror("Setting Pixel Format");
+    if (pthread_create(&capture_thread, NULL, capture_loop, NULL)) {
+        perror("pthread_create");
         return 1;
     }
-
-    memset(&streamparm, 0, sizeof(streamparm));
-    streamparm.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    streamparm.parm.capture.timeperframe.numerator = 1;
-    streamparm.parm.capture.timeperframe.denominator = fps;
-
-    xioctl(fd, VIDIOC_S_PARM, &streamparm);
-
-    memset(&req, 0, sizeof(req));
-    req.count = 1;
-    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    req.memory = V4L2_MEMORY_MMAP;
-
-    if (xioctl(fd, VIDIOC_REQBUFS, &req) == -1) {
-        perror("Requesting Buffer");
-        return 1;
-    }
-
-    memset(&buf, 0, sizeof(buf));
-    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buf.memory = V4L2_MEMORY_MMAP;
-    buf.index = 0;
-
-    if (xioctl(fd, VIDIOC_QUERYBUF, &buf) == -1) {
-        perror("Querying Buffer");
-        return 1;
-    }
-
-    buffer_length = buf.length;
-    buffer_start = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
-
-    if (buffer_start == MAP_FAILED) {
-        perror("mmap");
-        return 1;
-    }
-
-    if (xioctl(fd, VIDIOC_QBUF, &buf) == -1) {
-        perror("Queue Buffer");
-        return 1;
-    }
-
-    enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    if (xioctl(fd, VIDIOC_STREAMON, &type) == -1) {
-        perror("Start Capture");
-        return 1;
-    }
-
-    stop_capturing = 0;
-    pthread_create(&capture_thread, NULL, capture_loop, NULL);
 
     return 0;
 }
 
 int input_stop() {
-    stop_capturing = 1;
+    g_running = 0;
     pthread_join(capture_thread, NULL);
+
     enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     xioctl(fd, VIDIOC_STREAMOFF, &type);
-    munmap(buffer_start, buffer_length);
+
+    for (unsigned int i = 0; i < n_buffers; ++i) {
+        munmap(buffers[i].start, buffers[i].length);
+    }
+    free(buffers);
     close(fd);
     return 0;
 }
 
-int input_cmd(int plugin, unsigned int control, int value) {
-    return 0;
-}
-
 void input_help() {
-    fprintf(stderr, " -d | --device <device>     : video device (default: /dev/video0)\n");
-    fprintf(stderr, " -r | --resolution <WxH>    : resolution (default: 640x480)\n");
-    fprintf(stderr, " -f | --fps <fps>           : frames per second (default: 30)\n");
-    fprintf(stderr, " -y                         : force YUYV format\n");
+    fprintf(stderr, "Options for input_v4l2:\n"
+                    "  -d <device>     : video device (default: /dev/video0)\n"
+                    "  -r <WxH>        : resolution (default: 640x480)\n"
+                    "  -f <fps>        : frame rate (default: 30)\n"
+                    "  -y              : use YUYV format instead of MJPEG\n");
 }
 
-int input_parse_input(char *line) {
-    char *arg = strtok(line, " ");
-    while (arg) {
-        if (strcmp(arg, "-d") == 0 || strcmp(arg, "--device") == 0) {
-            arg = strtok(NULL, " ");
-            if (arg) device = strdup(arg);
-        } else if (strcmp(arg, "-r") == 0 || strcmp(arg, "--resolution") == 0) {
-            arg = strtok(NULL, " ");
-            if (arg) sscanf(arg, "%dx%d", &width, &height);
-        } else if (strcmp(arg, "-f") == 0 || strcmp(arg, "--fps") == 0) {
-            arg = strtok(NULL, " ");
-            if (arg) fps = atoi(arg);
-        } else if (strcmp(arg, "-y") == 0) {
-            force_yuyv = 1;
-        }
-        arg = strtok(NULL, " ");
-    }
-    return 0;
-}
+
